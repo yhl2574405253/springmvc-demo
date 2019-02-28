ca43809ee4347f5b8d4c49ed924b3451885547a4
yhl2574405253 <2574405253@qq.com>
2019-02-28 11:36:08 +0800

    提交springmvc消息转换器

--- src/main/java/cn/et/demo06/controller/FoodController.java
+++ src/main/java/cn/et/demo06/controller/FoodController.java
@@ -5,6 +5,7 @@ import net.sf.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -16,12 +17,46 @@ public class FoodController {
     @Autowired
     FoodMapper foodMapper;
 
+    /**
+     * 最原始的方法 通过outputStream输出
+     * @param os
+     * @param name
+     * @return
+     * @throws IOException
+     */
     @RequestMapping("/demo06/test1")
-    public String foodList(OutputStream os, String name) throws IOException {
+    public String test1(OutputStream os, String name) throws IOException {
         List<Map<String, Object>> queryAllFood =foodMapper.queryAllFood(name);
         JSONArray arry =JSONArray.fromObject(queryAllFood);
         String jsonStr =arry.toString();
         os.write(jsonStr.getBytes("UTF-8"));
         return null;
+    }
+
+    /**
+     * 用@ResponseBody的方式直接返回,减少流的输出动作
+     * @param name
+     * @return
+     * @throws IOException
+     */
+    @ResponseBody
+    @RequestMapping("/demo06/test2")
+    public byte [] test2(String name){
+        List<Map<String, Object>> queryAllFood =foodMapper.queryAllFood(name);
+        JSONArray arry =JSONArray.fromObject(queryAllFood);
+        String jsonStr =arry.toString();
+        return jsonStr.getBytes();
+    }
+
+    /**
+     * 配置消息转换器 springmvc自己帮你转换成json格式
+     * @param name
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/demo06/test3")
+    public List<Map<String, Object>> test3(String name){
+        List<Map<String, Object>> queryAllFood =foodMapper.queryAllFood(name);
+        return queryAllFood;
     }
 }
